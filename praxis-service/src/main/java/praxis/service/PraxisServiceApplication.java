1f375be6da7a56d0ea75e05ab5390cd5b2f3a7b6
gregwhitaker <gwhitake@gmail.com>
2019-11-25 17:27:00 -0800

    Doing work

--- praxis-service/src/main/java/praxis/service/PraxisServiceApplication.java
+++ praxis-service/src/main/java/praxis/service/PraxisServiceApplication.java
@@ -17,9 +17,17 @@ package praxis.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        FlywayAutoConfiguration.class})
 @EnableWebFlux
 public class PraxisServiceApplication {
 
