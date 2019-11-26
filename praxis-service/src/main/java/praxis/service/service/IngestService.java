377c3509bd56a8a9e7767ec4b5149f7a2ad9c2b6
gregwhitaker <gwhitake@gmail.com>
2019-11-25 16:58:03 -0800

    Doing work

--- praxis-service/src/main/java/praxis/service/service/IngestService.java
+++ praxis-service/src/main/java/praxis/service/service/IngestService.java
@@ -2,9 +2,14 @@ package praxis.service.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import praxis.service.data.ping.IngestLedgerDao;
 
 @Component
 public class IngestService {
     private static final Logger LOG = LoggerFactory.getLogger(IngestService.class);
+
+    @Autowired
+    private IngestLedgerDao ingestLedgerDao;
 }
