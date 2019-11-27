90a2a6ab30ab3c3fa0b0e9a9990c7454e837096d
gregwhitaker <gwhitake@gmail.com>
2019-11-27 11:08:26 -0800

    Doing work

--- praxis-service/src/main/java/praxis/service/controller/EventController.java
+++ praxis-service/src/main/java/praxis/service/controller/EventController.java
@@ -17,6 +17,7 @@ package praxis.service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,11 @@ public class EventController {
 
     @Autowired
     private EventService eventService;
+
+    @GetMapping("/events/public_keys")
+    public Mono<ResponseEntity> getKeys() {
+        return null;
+    }
 
     @PostMapping("/events")
     public Mono<ResponseEntity> ingestEvent(@RequestBody byte[] body) {
