428d22b85db28bf96bcf11bf546ee6f2c8e3ec27
gregwhitaker <gwhitake@gmail.com>
2019-11-27 17:27:20 -0800

    Doing work

--- praxis-service/src/main/java/praxis/service/config/PraxisConfiguration.java
+++ praxis-service/src/main/java/praxis/service/config/PraxisConfiguration.java
@@ -2,12 +2,15 @@ package praxis.service.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import praxis.service.config.settings.PraxisSettings;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 @EnableConfigurationProperties({
@@ -16,8 +19,13 @@ import java.nio.file.Path;
 public class PraxisConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(PraxisConfiguration.class);
 
+    @Autowired
     @Bean(name = "homeDir")
-    public Path homeDir() {
-        return null;
+    public Path homeDir(PraxisSettings settings) throws Exception {
+        if (Files.notExists(Paths.get(settings.getHomeDirectory()))) {
+            Files.createDirectories(Paths.get(settings.getHomeDirectory()));
+        }
+
+        return Paths.get(settings.getHomeDirectory());
     }
 }
