e2afeb479a57bc890c0dfc2f59eadfd1bb0e1d7f
gregwhitaker <gwhitake@gmail.com>
2019-11-30 09:01:34 -0800

    Doing work

--- praxis-java/src/test/java/praxis/client/internal/EventHandlerTest.java
+++ praxis-java/src/test/java/praxis/client/internal/EventHandlerTest.java
@@ -1,15 +1,57 @@
 package praxis.client.internal;
 
-import org.junit.BeforeClass;
-import org.mockserver.mockserver.MockServer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.verify.VerificationTimes;
+import praxis.client.PraxisConfiguration;
+import praxis.client.model.UserDefinedEvent;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 public class EventHandlerTest {
-    // TODO: setup mock server
 
-    @BeforeClass
-    public void setup() {
-        MockServer mockServer = new MockServer(9090);
+    @Rule
+    public MockServerRule mockServerRule = new MockServerRule(this);
+
+    private MockServerClient mockServerClient;
+
+    @Test
+    public void shouldCallPraxisService() throws Exception {
+        // Setup mock Praxis events endpoint
+        mockServerClient
+                .when(
+                        request("/events")
+                                .withMethod("POST")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(202)
+                );
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        UserDefinedEvent userDefinedEvent = new UserDefinedEvent.Builder().build();
+
+        Event event = new Event();
+        event.setWrappedEvent(userDefinedEvent);
+
+        PraxisConfiguration config = new PraxisConfiguration();
+        config.setHostname(mockServerClient.remoteAddress().getHostName());
+        config.setPort(mockServerClient.remoteAddress().getPort());
+
+        EventHandler eventHandler = new EventHandler(config);
+        eventHandler.onEvent(event, 1, true);
+
+        // Ensure that the event handler calls the Praxis endpoint
+        mockServerClient
+                .verify(
+                        request("/events")
+                                .withMethod("POST"),
+                        VerificationTimes.once()
+                );
     }
-
-    // TODO: validate that the call to praxis is correct
 }
