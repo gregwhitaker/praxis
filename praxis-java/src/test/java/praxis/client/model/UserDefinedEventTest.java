149ff304863f6990d37dc067b5bdf91f4e2d4d52
gregwhitaker <gwhitake@gmail.com>
2019-11-29 22:09:11 -0800

    Doing work

--- praxis-java/src/test/java/praxis/client/model/UserDefinedEventTest.java
+++ praxis-java/src/test/java/praxis/client/model/UserDefinedEventTest.java
@@ -20,4 +20,23 @@ public class UserDefinedEventTest {
 
         Assert.assertEquals(123L, event.attributes.get("userEventType"));
     }
+
+    @Test
+    public void shouldSetFieldsWhenCorrelatedEventAdded() {
+        StartupEvent startupEvent = new StartupEvent.Builder()
+                .application("foo")
+                .instance("fooInstance")
+                .environment("test")
+                .build();
+
+        UserDefinedEvent event = new UserDefinedEvent.Builder()
+                .correlatedEvent(startupEvent)
+                .userEventType(123)
+                .build();
+
+        Assert.assertEquals(startupEvent.getId(), event.getCorrelatedId());
+        Assert.assertEquals("foo", event.getApplication());
+        Assert.assertEquals("fooInstance", event.getInstance());
+        Assert.assertEquals("test", event.getEnvironment());
+    }
 }
