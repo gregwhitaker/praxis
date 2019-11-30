e51bf0c3591d625108b0be3c404f4190eaa443aa
gregwhitaker <gwhitake@gmail.com>
2019-11-29 22:28:09 -0800

    Doing work

--- praxis-java/src/test/java/praxis/client/internal/EventHandlerTest.java
+++ praxis-java/src/test/java/praxis/client/internal/EventHandlerTest.java
@@ -1,7 +1,15 @@
 package praxis.client.internal;
 
+import org.junit.BeforeClass;
+import org.mockserver.mockserver.MockServer;
+
 public class EventHandlerTest {
     // TODO: setup mock server
+
+    @BeforeClass
+    public void setup() {
+        MockServer mockServer = new MockServer(9090);
+    }
 
     // TODO: validate that the call to praxis is correct
 }
